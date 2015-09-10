ed19ec07aabd11e6385f61720af101c671b29d72
kingsley <hryzc247yixin@163.com>
2015-09-10 10:15:34 +0800

    远程读取

--- github/github/Program.cs
+++ github/github/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Management;
+using System.IO;
 
 namespace github
 {
@@ -10,7 +12,27 @@ namespace github
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello! world");
+            ConnectionOptions conn = new ConnectionOptions();
+            conn.Username = "kingsley";//登入远端电脑的账户
+            conn.Password = "111111";//登入远端电脑的密码
+            ManagementPath path = new ManagementPath();
+
+            //其中root\cimv2是固定写法
+            path.Path = @"\\10.0.1.36\root\cimv2";
+            ManagementScope ms = new ManagementScope();
+            ms.Options = conn;
+            ms.Path = path;
+            ms.Connect();
+            //这里的例子读取文件的最后修改日期
+            ObjectQuery query = new ObjectQuery("SELECT * FROM CIM_DataFile WHERE Name = 'C:\\\\KVOffice.log'");
+            ManagementObjectSearcher  searcher = new ManagementObjectSearcher(ms,query);
+            ManagementObjectCollection collection = searcher.Get();
+            string time = "";
+            foreach (ManagementObject obj in collection)
+            {
+                time = obj.Properties["LastModified"].Value.ToString().Substring(0,14);
+            }
+
         }
     }
 }
